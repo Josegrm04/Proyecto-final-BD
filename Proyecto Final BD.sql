c410f8e6afdfecb79a0870379f3cca6a039523b4
Josegrm04 <104604947+Josegrm04@users.noreply.github.com>
2022-08-27 16:58:50 -0400

    Create Proyecto Final BD.sql

---
+++ Proyecto Final BD.sql
@@ -0,0 +1,72 @@
+Jose Gabriel Ramirez Mendez
+21-EIIN-1-069
+0541
+Starling Germosen Reynoso
+
+create database Unit_sales
+
+use Unit_sales
+
+Create table Ventas diarias (
+  Prom Cantidad de articulos Char (10) Not null,
+  pagos con tarjetas char (10) not null,
+  Pagos efectivo char (10) not null,
+  Ventas totales (10) not null,
+  );
+  
+insert into Ventas diarias (Prom Cantidad de articulos, pagos con tarjetas, Pagos efectivo, ventas totales) values ("35", "26", "9", "1250695")
+
+insert into Ventas diarias (Prom Cantidad de articulos, pagos con tarjetas, Pagos efectivo, ventas totales) values ("845", "459", "386", "3956523")
+
+insert into Ventas diarias (Prom Cantidad de articulos, pagos con tarjetas, Pagos efectivo, ventas totales) values ("1796", "1000", "796", "6475557")
+
+insert into Ventas diarias (Prom Cantidad de articulos, pagos con tarjetas, Pagos efectivo, ventas totales) values ("4987", "2478", "2509", "8494855")
+
+insert into Ventas diarias (Prom Cantidad de articulos, pagos con tarjetas, Pagos efectivo, ventas totales) values ("2879", "1596", "1283", "2449693")
+
+create table ventas mensuales (
+
+  Enero char (10) Not null,
+  Febrero char (10) not null,
+  Marzo char (10) not null,
+  Abril char (10) not null,
+  Mayo char (10) not null,
+  Junio char (10) not null,
+  Julio char (10) not null, 
+  Agosto char (10) not null,
+  );
+  
+  insert into ventas mensuales ( Enero) values ("$RD 15265000")
+  
+  insert into ventas mensuales (Febrero) values ("$RD 13498755")
+  
+  insert into ventas mensuales (Marzo) values ("$RD 16984565")
+  
+  insert into ventas mensuales (Abril) values ("$RD 12959553")
+  
+  insert into ventas mensuales (Mayo) values ("$RD 14856266")
+  
+  insert into ventas mensuales (Junio) values ("$RD 17965274")
+  
+  insert into ventas mensuales (Julio) values ("$RD 11964987")
+  
+  insert into ventas mensuales (Agosto) values ("$RD 10648496")
+  
+  );
+  
+  create table ventas anuales 2019-2022 (
+   2019 char (10) not null,
+   2020 char (10) not null,
+   2021 char (10) not null.
+   2022 char (10) not null,
+    
+    insert into ventas anuales 2019-2022 (2019) values ("$RD 198293328")
+    
+    insert into ventas anuales 2019-2022 (2020) values ("$RD 186365465")
+    
+    insert into ventas anuales 2019-2022 (2021) values ("$RD 179962525")
+    
+    insert into ventas anuales 2019-2022 (2022) values ("$RD 194694626")
+        
+    );
+  
